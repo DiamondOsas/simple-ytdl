b6969be46300b54a0357937f087a587f4d874b45
DiamondOsas <diamondosazuwa@gmail.com>
2026-01-07 18:09:52 +0100

    Comitiing

--- src/yt/download.py
+++ src/yt/download.py
@@ -1,9 +1,9 @@
 import yt_dlp
-
+print(yt_dlp.YoutubeDL.__doc__)
 def download_video(url, quality):
 
     options = {'quiet' : True}
     downloader = yt_dlp.YoutubeDL(options)
-
+    
     # try:
     #     download_video = downloader.download()
